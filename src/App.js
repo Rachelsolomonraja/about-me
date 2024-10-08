73a1786fe1012ff7425ea70193c521cf6400ba25
Rachelsolomonraja <rachelsolomonraja477@gmail.com>
2024-10-08 15:25:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>Rachel</h1>
+    <h3>Web Developer</h3>
+    <hr />
+      
+      <h5>Sirkali, Mayiladuthurai</h5>
+      <h5>rachelsolomonraja477@gmail.com</h5>
+      <h5>8098526127</h5>
+      <hr />
+      <h3>PROFESSIONAL SUMMARY</h3>
+      <p>
+      Highly motivated and enthusiatic individual, seeking a challenging role to kickstart my career, proficient in foundation in datascience. Eager to learn,adapt and contribute to innovative projects.I am qualified and professional web developer.Seeking an entry-level opportunity with an esteemed organization where I can utilize my skils and enhance learning in the field of work. Capable of mastering new technologies. 
+      </p>
+      <hr/>
+      <h3>TECHNICAL SKILLS</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Git</li>
+        <li>Github</li>
+      </ul>
+      <hr/>
+      <h3>EDUCATION</h3>
+      <h4>Bachelor of Theology</h4>
+      <div>
+      <h5>Lakeview Bible College and Seminary</h5>
+      <div>
+      <h6>Bachelor of Engineering</h6>
+      <div>
+      <h7>Vetri Vinayaha College of Engineering and Technology</h7>
+      </div>
+      </div>
+      </div>
+      <hr/>
+      <h3>SKILLS</h3>
+      <ul>
+        <li>Leadership</li>
+        <li>Communication</li>
+        <li>Self-confident</li>
+      </ul>
+      <hr/>
+      <h3>PROJECTS</h3>
+      <h4>portfolio</h4>
+      <div>
+      <p>
+      The portfolio is devloped using reactjs a popular javascript library for building user interface.Additional technology include HTML5,CSS3.
+      </p>
+      </div>  
+        <hr/>
+      <h3>CERTIFICATION COURSE</h3>
+      <h4>Full Stack Development</h4>
+      <h5>NoviTech, Coimbatore</h5>
+    
     </div>
   );
 }
